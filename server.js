60f20c48d75c245a6d1dd08d7b04d4ebb99fb11f
Sanjay <ssanjaysuresh2003@gmail.com>
2025-11-13 10:50:30 +0530

    updated files

--- server.js
+++ server.js
@@ -7,9 +7,16 @@ const admin = require('firebase-admin');
 dotenv.config();
 
 const app = express();
-app.use(cors());
-app.use(express.json());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://projecthub-frontend-alpha.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+}));
 
+app.use(express.json());
 try {
   const serviceAccount = require('./config/serviceAccountKey.json');
   admin.initializeApp({
